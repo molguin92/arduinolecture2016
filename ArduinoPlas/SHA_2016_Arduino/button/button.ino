634ec5b32d044e7ee992e79461fe46a76581064f
Manuel Olguin <molguin@dcc.uchile.cl>
2016-01-10 21:13:19 -0300

    adding stuff

---
+++ ArduinoPlas/SHA_2016_Arduino/button/button.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(7, INPUT);
+}
+
+void loop() {
+  int button = digitalRead(7);
+  if(button == HIGH)
+  {
+    digitalWrite(8, HIGH);
+    delay(50);
+    digitalWrite(8, LOW);
+    delay(50);
+    digitalWrite(8, HIGH);
+  }
+  else
+  {
+    digitalWrite(8, LOW);
+  }
+}
